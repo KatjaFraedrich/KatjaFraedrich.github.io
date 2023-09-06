887539df53a585fc52f1fbc3e6e49962d6ebfd1a
KatjaFraedrich <katja.fraedrich@uni-konstanz.de>
2023-09-06 17:30:45 +0200

    changed to own project

--- KatjaFraedrich.github.io/dist/index.js
+++ KatjaFraedrich.github.io/dist/index.js
@@ -1,79 +1,130 @@
 import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-const canvas = document.querySelector('canvas.webgl')
+
 
 // Scene
 const scene = new THREE.Scene()
+scene.background=0xff0000
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-const textureLoader = new THREE.TextureLoader()
-const myTexture = textureLoader.load('coolTex.jpg')
-
-// Object
-const geometry = new THREE.BoxGeometry(1,1,1)
-const geometry2 = new THREE.DodecahedronGeometry(0.5,3)
-const material = new THREE.MeshBasicMaterial({
-    map: myTexture
+window.addEventListener('resize', () => {
+    onWindowResize(); // your function?
+    });
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('canvas.webgl'),
+    alpha:true
 })
-const boxMesh = new THREE.Mesh(geometry,material)
-const sphereMesh = new THREE.Mesh(geometry2,material)
-scene.add(boxMesh)
-// scene.add(sphereMesh)
-boxMesh.position.x = 0
-boxMesh.position.y = 0.8
-sphereMesh.position.x = -1.6
-sphereMesh.position.y = 0.5
-geometry.center()
-// Sizes
-const sizes = {
-    width:window.innerWidth,
-    height:window.innerHeight
-}
+renderer.setSize(width, height);
 
-// Renderer gets updated each time window is resized
-window.addEventListener('resize',()=>{
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+document.body.appendChild( renderer.domElement );
 
-    camera.aspect = sizes.width/sizes.height
-    camera.updateProjectionMatrix()
-
-    renderer.setSize(sizes.width,sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-    
-})
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.z = 30
 scene.add(camera)
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
 
-controls.enableZoom = false;
-controls.enableDamping = true
+const clock=new THREE.Clock()
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true,
+
+
+//renderer.setClearColor( 0x000000, 0 ); // 
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+
+
+
+
+
+// Object
+const geometry = new THREE.BoxGeometry(16, 16, 16,16,16,16)
+const newMaterial= new THREE.MeshStandardMaterial({ color: 0xff0000 ,wireframe:true})
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const uniformData={
+    u_time:{type:'f', value: clock.getElapsedTime(),}
+}
+
+const shaderMat=new THREE.ShaderMaterial({
+    //wireframe:true,
+    uniforms: uniformData,   //uniform data can be manipulated from the outside (e.g. a time or other global settings)
+                            //vaying data is passed from the vertexshader to the fragmentshader (e.g. the position or computed vars)
+    vertexShader: `         
+    varying vec3 pos;
+    uniform float u_time;
+
+    void main() {
+        vec4 result;
+        pos=position;
+        result=vec4(position.x,4.0*sin(position.z/4.0+u_time)+position.y,position.z,1.0);
+        gl_Position=projectionMatrix * modelViewMatrix * result;
+    }`,
+    fragmentShader: `
+    varying vec3 pos;
+    void main() {
+        if(pos.x>=0.0){
+            gl_FragColor=vec4(1.0,0.0,0.0,1.0);
+        } 
+        else{
+            gl_FragColor=vec4(0.0,1.0,0.0,1.0);
+        }
+        
+    }`
 })
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+const mesh = new THREE.Mesh(geometry, shaderMat)
+scene.add(mesh)
 
-const clock = new THREE.Clock()
 
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-    boxMesh.rotateX(30*0.0003)
-    boxMesh.rotateY(30*0.0003)
-    sphereMesh.rotateY(30*0.0003)
-    // mesh.position.y = Math.sin(elapsedTime) *0.1
-    boxMesh.position.z = Math.sin(elapsedTime) * 1
 
-    controls.update()
-    controls.enableDamping = true
-    renderer.render(scene,camera)
+
+
+
+
+
+
+
+
+
+
+renderer.render(scene, camera)
+
+/**
+ * Animate
+ */
+let time = Date.now()
+
+const tick = () =>
+{
+
+    // Time
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+    uniformData.u_time.value=clock.getElapsedTime();
+    // Update objects
+    //mesh.rotation.y += 0.0001 * deltaTime
+    controls.update();
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
-};
+}
 
 tick()
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    render();
+
+}
+
